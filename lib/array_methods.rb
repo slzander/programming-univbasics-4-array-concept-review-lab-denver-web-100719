4cff435299bc1d9d5f54869a5e5673690a68b896
Stacey Zander <staceyzander@gmail.com>
2019-09-05 17:56:29 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,5 +1,13 @@
 def find_element_index(array, value_to_find)
-  
+  counter = 0 
+  while array[counter] do
+    if array[counter] == value_to_find
+      return counter
+    else
+      counter += 1 
+    end
+  end
+  return nil
 end
 
 def find_max_value(array)
